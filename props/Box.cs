cfafa81031442fdeca5f420cb123c8edf100bdb3
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-25 17:20:00 +0100

    Add project files.

---
+++ props/Box.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace props
+{
+    internal class Box
+    {
+        //constructor
+        public Box(int length,int heigth,int width)
+        {
+            this.length = length;   
+            this.heigth = heigth;
+            this.Width=width;
+        }
+
+
+        //member variables
+        private int length =3;
+        private int heigth;
+        //public int width; you dont have to have member property, you van just use the props -3-
+        private int volume;
+        
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length = {0}, heigth = {1}, width = {2}, volume = {3}",length,heigth,Width,GetVolume()); //Width, because is not setup thru member variables
+        }
+
+        //setting thruought method -1-
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+        public int GetLength()
+        {
+            return this.length;
+        }
+        public int GetVolume()
+        {
+            return length * Width * heigth;
+        }
+
+        //setting thruought props -2-
+       public int Height
+        {
+            get { return heigth; }
+            set { heigth = value; }
+        }
+        //-3-
+        public int Width { get; set; }//default getter, setter
+
+        public int Volume
+        {
+            get { return this.length*this.heigth*this.Width;  }
+            
+        }
+
+        public int FrontSurface
+        {
+            get { return this.heigth * this.length; }
+        } 
+    }
+}
---
+++ props/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace props
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Box box = new(10,10,30);
+
+            ////using prop -2-
+            //box.Height = 4;
+            //box.Width = 5; 
+            //commented out because of use of the constuctor
+
+
+            //using function -1-
+            box.SetLength(4);
+            //box.lenght - no access because is private, we need to setup the get in the class to nbe able to access this
+            int length = box.GetLength();
+            Console.WriteLine(length);
+
+
+            box.DisplayInfo();
+
+            //returning Volume from prop
+            Console.WriteLine("Volume = {0}", box.Volume);
+            Console.WriteLine("The front surface of the Box is: {0}",box.FrontSurface);
+
+            Console.ReadLine();
+
+        }
+    }
+}
